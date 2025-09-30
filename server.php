eeff60bbc57651e1f71e7903108eb560eaa09d3d
Michael354123 <ssm1009631@est.univalle.edu>
2025-09-29 23:44:01 -0400

    proyecto prueba

--- server.php
+++ server.php
@@ -1,12 +1,13 @@
 <?php
-// Datos de conexión a la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "ssm354123";
-$dbname = "perfexcrm";
+// Datos de conexión a la base de datos desde variables de entorno
+$servername = getenv("DB_HOST");
+$username   = getenv("MYSQLUSER");        // Railway suele dar MYSQLUSER
+$password   = getenv("MYSQLPASSWORD");    // Railway da MYSQLPASSWORD
+$dbname     = getenv("MYSQL_DATABASE");   // Railway da MYSQL_DATABASE
+$port       = getenv("MYSQLPORT");        // Railway da MYSQLPORT
 
 // Crear una conexión a la base de datos
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Verificar la conexión
 if ($conn->connect_error) {
